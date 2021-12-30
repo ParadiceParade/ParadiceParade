d7220517c1f36d4c6b280d91e028291db46a8aa9
Chidi Benedict <c4benni@gmail.com>
2021-12-30 19:48:46 +0100

    updated SizeGrid

--- components/SizeRadio.vue
+++ components/SizeRadio.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4 mt-4">
+  <div
+    role="radiogroup"
+    class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4 mt-4"
+  >
     <label
       v-for="(item, i) in items"
       :key="i"
       :for="`color-radio-${item.title}`"
-      class="group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 bg-white dark:bg-gray-900 shadow-sm text-gray-900 cursor-pointer undefined"
+      class="group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 dark:hover:bg-gray-800 focus:outline-none sm:flex-1 sm:py-6 bg-white dark:bg-gray-900 shadow-sm text-gray-900 dark:text-gray-100 cursor-pointer undefined"
       :class="{
-        'ring-gray-400 dark:ring-gray-700 ring ring-offset-1':
+        'ring-primary-700 dark:ring-primary-500 ring ring-offset-1':
           item.title == activeItem.title
       }"
     >
@@ -25,7 +28,8 @@
       <span
         class="absolute -inset-px rounded-md pointer-events-none border-2"
         :class="{
-          'border-transparent': item.title != activeItem.title,
+          'border-black dark:border-white dark:border-opacity-10 border-opacity-10':
+            item.title != activeItem.title,
           'border-primary-700 dark:border-primary-500':
             item.title == activeItem.title
         }"
