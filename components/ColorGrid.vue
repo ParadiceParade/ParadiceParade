2d29bf8b59290311f11d8cb00c4186a8b87fc378
Chidi Benedict <c4benni@gmail.com>
2021-12-30 19:00:46 +0100

    added ColorGrid

---
+++ components/ColorGrid.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <label
+      v-for="(item, i) in items"
+      :key="i"
+      :for="`color-radio-${item.value}`"
+      class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none ring-gray-400 dark:ring-gray-700 ring ring-offset-1"
+    >
+      <input
+        :id="`color-radio-${item.value}`"
+        type="radio"
+        :value="item.value"
+        class="sr-only"
+      />
+
+      <span
+        class="h-8 w-8 rounded-full border border-black dark:border-white border-opacity-10"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorGrid',
+
+  model: {
+    props: 'vmodel',
+    event: 'onvmodel'
+  },
+
+  props: {
+    vmodel: {
+      type: String,
+      default: ''
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  computed: {
+    activeItem() {
+      return this.vmodel || this.items[0].value
+    }
+  }
+}
+</script>
+
+<style></style>
