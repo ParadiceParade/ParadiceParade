a15267623a82a03a858f79eac9340f15a875c856
Chidi Benedict <c4benni@gmail.com>
2021-12-30 19:42:03 +0100

    updated SizeRadio

--- components/SizeRadio.vue
+++ components/SizeRadio.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid grid-flow-col gap-x-4 py-4 justify-start">
+  <div class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4 mt-4">
     <label
       v-for="(item, i) in items"
       :key="i"
       :for="`color-radio-${item.title}`"
-      class="group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 bg-white shadow-sm text-gray-900 cursor-pointer undefined"
+      class="group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 bg-white dark:bg-gray-900 shadow-sm text-gray-900 cursor-pointer undefined"
       :class="{
         'ring-gray-400 dark:ring-gray-700 ring ring-offset-1':
           item.title == activeItem.title
