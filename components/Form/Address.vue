476de6f700405d800578787d5e2939583160fc67
Chidi Benedict <c4benni@gmail.com>
2022-01-11 20:24:07 +0100

    Update Address.vue

--- components/Form/Address.vue
+++ components/Form/Address.vue
@@ -1,6 +1,18 @@
 <template>
     <section>
-        <FormSavedAddress/>
+        <LazyFormSavedAddress title="Choose an address"/>
+
+        <Divider class="my-8 lg:my-12 text-gray-700 dark:text-gray-300">
+            OR
+        </Divider>
+
+        <h2
+            class="text-xl mb-4"
+        >
+            Create an address
+        </h2>
+
+        <FormCreateAddress/>
     </section>
 </template>
 
