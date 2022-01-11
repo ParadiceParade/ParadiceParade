6e90a94e6013e4f4aee7b9b2f01758e5f18f20d9
Chidi Benedict <c4benni@gmail.com>
2022-01-11 20:23:37 +0100

    Create Divider.vue

---
+++ components/Divider.vue
@@ -0,0 +1,13 @@
+<template>
+    <div
+        class="root"
+    >
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+    .root{
+        @apply grid items-center grid-flow-col grid-cols-[1fr,auto,1fr] gap-x-2 before:w-full before:block after:block after:w-full before:h-[0.75px] after:h-[0.75px] before:bg-gray-300 dark:before:bg-gray-700 after:bg-gray-300 dark:after:bg-gray-700;
+    }
+</style>
