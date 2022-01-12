8fb0537879e4e74dd5ceddf01bfe4f72eb975b7a
Chidi Benedict <c4benni@gmail.com>
2022-01-12 19:59:00 +0100

    Update Button.vue

--- components/Button.vue
+++ components/Button.vue
@@ -10,6 +10,7 @@ export default {
     primary: Boolean,
     block: Boolean,
     disabled: Boolean,
+    hidePlainFocus:Boolean,
     tag: {
       type: String,
       default: 'button'
@@ -73,6 +74,8 @@ export default {
                 icon: this.icon,
 
                 primary: this.primary,
+                'no-bg': !this.primary,
+                'hide-plain-focus':  this.hidePlainFocus,
 
                 'border-[0.75px] border-white border-opacity-10 bg-white bg-opacity-10 hover:bg-opacity-20':
                   this.overlay
@@ -104,9 +107,13 @@ export default {
   @apply after:w-full after:absolute after:h-[1px] after:bg-primary-700 dark:after:bg-primary-500 after:opacity-0 after:bottom-0 after:translate-x-[-100%] hover:after:translate-x-0 hover:after:opacity-70 after:transform-gpu after:transition-all;
 }
 .root[data-btn] {
-  @apply inline-grid grid-flow-col gap-x-2 relative overflow-hidden isolate items-center justify-center cursor-pointer transform-gpu active:opacity-80 active:scale-[0.9925];
+  @apply inline-grid grid-flow-col gap-x-2 relative overflow-hidden isolate items-center justify-center cursor-pointer transform-gpu active:opacity-90 active:scale-[0.975] sm:active:scale-[0.9925];
+  transition: 0.35s ease;
   transition-property: opacity, transform;
-  transition: 0.15s ease;
+}
+
+.root[data-btn]:hover{
+  will-change: opacity, transform;
 }
 
 .link-btn[data-btn] {
@@ -116,10 +123,17 @@ export default {
 }
 
 .ui-btn[data-btn] {
-@apply outline-none focus-visible:ring-[2px] ring-offset-white dark:ring-offset-black
+@apply outline-none ring-offset-white dark:ring-offset-black
 ring-offset-1 ring-primary-900 dark:ring-primary-700 font-semibold;
-
   width: fit-content;
+}
+
+.ui-btn[data-btn]:not(.no-bg){
+  @apply focus-visible:ring-[2px];
+}
+
+.ui-btn[data-btn].no-bg:not(.hide-plain-focus){
+  @apply focus:ring-[2px];
 }
 
 .sm[data-btn] {
