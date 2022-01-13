bad495a1d0d381f3b76808f876146ba9ccd6d10f
Chidi Benedict <c4benni@gmail.com>
2022-01-13 22:52:24 +0100

    Create Shipping.vue

---
+++ components/Checkout/Shipping.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <LazyCheckoutDeliveryInfo
+            :contents="deliveryInfo"
+        />
+    </section>
+</template>
+
+<script>
+   export default {
+       data:()=>({
+           deliveryInfo:[
+               {
+                   title: 'Recipient',
+                   content: 'ihejirikabenchidi@gmail.com'
+               },
+               {
+                   title: 'Address',
+                   content: 'Raji Apoyin, 16, 100001 Lagos EN, Nigeria'
+               }
+           ]
+       })
+   }
+</script>
