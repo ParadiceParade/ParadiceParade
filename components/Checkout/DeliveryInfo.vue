a71b68fd6363660c61407bd1ba69592f3d5e8304
Chidi Benedict <c4benni@gmail.com>
2022-01-13 22:52:20 +0100

    Create DeliveryInfo.vue

---
+++ components/Checkout/DeliveryInfo.vue
@@ -0,0 +1,33 @@
+<template>
+    <section v-if="contents">
+        <h2>   
+            Delivery info
+        </h2>
+
+        <address>
+
+            <div v-for="(info, i) in contents" :key="`${i}`">
+                <span>
+                    {{info.title}}
+                </span>
+
+                <span>
+                    {{info.content}}
+                </span>
+            </div>
+            
+        </address>
+    </section>
+</template>
+
+<script>
+   export default {
+       name: 'CheckoutDeliveryInfo',
+       props:{
+           contents:{
+               type: Array,
+               default: undefined
+           }
+       }
+   }
+</script>
