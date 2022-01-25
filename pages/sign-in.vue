c7fa9eb550951bcb206a4c1b9b10d9cfca1621ba
Chidi Benedict <c4benni@gmail.com>
2022-01-25 17:40:30 +0100

    Create sign-in.vue

---
+++ pages/sign-in.vue
@@ -0,0 +1,11 @@
+<template>
+    <article
+        class='grid justify-items-center items-center pt-16 pb-12 md:pt-20 md:pb-18 px-3'
+    >
+        <FormSignIn
+            class='max-w-[550px] w-full'
+            sign-up-link='/sign-up'
+            forgot-password-link="/forgot-password"
+        />
+    </article>    
+</template>
