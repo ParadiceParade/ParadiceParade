cd3e4b6f0cffe663011f127ed26695e332f7920c
Chidi Benedict <c4benni@gmail.com>
2022-01-25 17:40:26 +0100

    Create forgot-password.vue

---
+++ pages/forgot-password.vue
@@ -0,0 +1,10 @@
+<template>
+    <article
+        class='grid justify-items-center items-center pt-16 pb-12 md:pt-20 md:pb-18 px-3'
+    >
+        <FormResetPassword
+            class='max-w-[550px] w-full'
+            sign-in-link='/sign-in'
+        />
+    </article>    
+</template>
